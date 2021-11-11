c1e1ace6a9e19fe3b17c375e598fcec4a81056b5
Hao Doan <hao.doan@lexisnexisrisk.com>
2021-11-11 17:00:09 +0700

    add key template for MAC

--- secret_generator.js
+++ secret_generator.js
@@ -1,10 +1,11 @@
 const TEMPLATE = "kubectl create secret generic {secretName} {data} -n {nameSpace}";
-const KEY_TEMPLATE = "--from-literal={key}={data}";
+const KEY_TEMPLATE_WIN = "--from-literal={key}={data}";
+const KEY_TEMPLATE_MAC = "--from-literal={key}='{data}'";
 
 function generateForWin(secretName, nameSpace, data) {
     let compeletedData = [];
     for (let k in data) {
-        let keyData = KEY_TEMPLATE
+        let keyData = KEY_TEMPLATE_WIN
             .replace("{key}", k)
             .replace("{data}", window.atob(data[k]));
         compeletedData.push(keyData);
@@ -18,7 +19,7 @@ function generateForWin(secretName, nameSpace, data) {
 function generateForMac(secretName, nameSpace, data) {
     let compeletedData = [];
     for (let k in data) {
-        let keyData = KEY_TEMPLATE
+        let keyData = KEY_TEMPLATE_MAC
             .replace("{key}", k)
             .replace("{data}", window.atob(data[k]));
         compeletedData.push(keyData);
