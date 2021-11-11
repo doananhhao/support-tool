11b751710c3359a23e864cacd809ac297dab46b2
Chien Cao <chien.cao@lexisnexisrisk.com>
2021-11-11 16:41:36 +0700

    support Mac

--- secret_generator.js
+++ secret_generator.js
@@ -1,7 +1,21 @@
 const TEMPLATE = "kubectl create secret generic {secretName} {data} -n {nameSpace}";
 const KEY_TEMPLATE = "--from-literal={key}={data}";
 
-function generate(secretName, nameSpace, data) {
+function generateForWin(secretName, nameSpace, data) {
+    let compeletedData = [];
+    for (let k in data) {
+        let keyData = KEY_TEMPLATE
+            .replace("{key}", k)
+            .replace("{data}", window.atob(data[k]));
+        compeletedData.push(keyData);
+    }
+    return TEMPLATE
+        .replace("{secretName}", secretName)
+        .replace("{data}", compeletedData.join(" "))
+        .replace("{nameSpace}", nameSpace);
+}
+
+function generateForMac(secretName, nameSpace, data) {
     let compeletedData = [];
     for (let k in data) {
         let keyData = KEY_TEMPLATE
